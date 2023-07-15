697c0ff58a6b7341f51274605916c1c4e9e7e64f
Julian Pasquale <julianagustin78@gmail.com>
2023-07-15 12:47:28 -0300

    Traducir fechas (#21)
    * Translate dates
    
    * Rename method

---
+++ _plugins/date_formatter.rb
@@ -0,0 +1,13 @@
+module Jekyll::DateFormatter
+  MONTHS = %w[Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre].freeze
+
+  def spanish_date(input)
+    day = input.strftime('%-d')
+    month = MONTHS[input.strftime('%m').to_i - 1]
+    year = input.strftime('%Y')
+
+    "#{day} de #{month} del #{year}"
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::DateFormatter)
